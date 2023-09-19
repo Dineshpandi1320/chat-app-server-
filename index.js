80763817092e9bd5a27ed29e67e174da990a5b8f
AbiyaElangovan <abielangovan1@gmail.com>
2023-09-20 00:02:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+
+const { Server} =require("socket.io")
+app.use(cors());
+
+const PORT = 3002;
+const server = http.createServer(app);
+
+
+
+
+const io = new Server (server,{
+    cors: {
+        origin:"*",
+        methods:["GET" ,"POST"],
+    },
+});
+
+
+
+io.on("connection",(socket) =>{
+    console.log(`User_connected ${socket.id}`);
+
+    socket.on("join_room",(data) =>{
+        socket.join(data);
+        console.log(`User with ID :${socket.id} joined room :${data} `)
+    });
+
+
+    socket.on("send_message",(data) =>{
+        socket.to(data.room).emit("received_message",data);
+    });
+
+
+    socket.on("disconnect",() => {
+        console.log("User Disconnected",socket.id);
+    });
+});
+
+server.listen(PORT,() =>{
+    console.log("SERVER RUNNING = " + PORT);
+});
